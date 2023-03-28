9e1bf6a9de00250ffb3f61f96236cbd16b4b9ad4
gaofeng <1024355591@qq.com>
2023-03-28 22:49:58 +0800

    [shell]test

--- target/neutronrc/f435se/config/module.py
+++ target/neutronrc/f435se/config/module.py
@@ -23,6 +23,20 @@ MODULES = [
     'pmu/*.c',
 ]
 
+MODULES += [
+    'syscmd/optparse.c',
+    'syscmd/syscmd.c',
+    'syscmd/cmd_ps.c',
+    'syscmd/cmd_test.c',
+    'syscmd/cmd_mcn.c',
+    'syscmd/cmd_param.c',
+    'syscmd/cmd_task.c',
+    'syscmd/cmd_rc.c',
+    'syscmd/cmd_adc.c',
+    'syscmd/cmd_act.c',
+    # 'syscmd/cmd_mlog.c',
+]
+
 MODULES_CPPPATH = [
     'calibration',
 ]
